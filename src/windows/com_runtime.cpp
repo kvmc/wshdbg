fbcf52576c02698c8d1c8f7fd8ab5e58b7fd4fdb
kvmc <kvmc@users.noreply.github.com>
2026-08-14 20:19:09 -0400

    Fix Windows COM runtime declarations

--- src/windows/com_runtime.cpp
+++ src/windows/com_runtime.cpp
@@ -1,5 +1,17 @@
 #include "com_runtime.hpp"
+#include <objbase.h>
+
 namespace wshdbg::windows {
-ComRuntime::ComRuntime(){ hr_=CoInitializeEx(nullptr,COINIT_APARTMENTTHREADED); owns_uninitialize_=SUCCEEDED(hr_); }
-ComRuntime::~ComRuntime(){ if(owns_uninitialize_) CoUninitialize(); }
+
+ComRuntime::ComRuntime() {
+    hr_ = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+    owns_uninitialize_ = SUCCEEDED(hr_);
 }
+
+ComRuntime::~ComRuntime() {
+    if (owns_uninitialize_) {
+        CoUninitialize();
+    }
+}
+
+} // namespace wshdbg::windows
