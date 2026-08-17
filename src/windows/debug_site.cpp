b06c30bce42eea12b3d72c2018e69cd5fc845e8f
kvmc <kvmc@users.noreply.github.com>
2026-08-17 19:53:04 -0400

    Implement Active Script event translation bridge

--- src/windows/debug_site.cpp
+++ src/windows/debug_site.cpp
@@ -1,15 +1,58 @@
-// Active Scripting debugger integration begins here.
-//
-// Responsibilities:
-// - IActiveScriptSiteDebug implementation
-// - Source document registration
-// - Breakpoint binding callbacks
-// - Debug event translation into wshdbg-core events
-
-#include "wshdbg/debugger.hpp"
+#include "debug_site.hpp"
 
 namespace wshdbg::windows {
 
-// Initial scaffold. COM debugger interfaces will be implemented incrementally.
-
+void DebugSiteBridge::script_loaded(const std::filesystem::path& script) {
+    session_.debugger_event(DebugEvent{
+        .type = DebugEventType::ScriptLoaded,
+        .script = script,
+        .message = L"script loaded"
+    });
 }
+
+void DebugSiteBridge::breakpoint_bound(BreakpointId id, SourceLocation location) {
+    session_.debugger_event(DebugEvent{
+        .type = DebugEventType::BreakpointBound,
+        .location = location,
+        .breakpoint_id = id,
+        .script = location.file,
+        .message = L"breakpoint bound"
+    });
+}
+
+void DebugSiteBridge::breakpoint_hit(std::optional<BreakpointId> id, SourceLocation location, std::wstring_view message) {
+    session_.debugger_event(DebugEvent{
+        .type = DebugEventType::BreakpointHit,
+        .location = location,
+        .breakpoint_id = id,
+        .script = location.file,
+        .message = std::wstring{message}
+    });
+}
+
+void DebugSiteBridge::step_complete(SourceLocation location) {
+    session_.debugger_event(DebugEvent{
+        .type = DebugEventType::StepComplete,
+        .location = location,
+        .script = location.file,
+        .message = L"step complete"
+    });
+}
+
+void DebugSiteBridge::script_error(SourceLocation location, std::wstring_view message) {
+    session_.debugger_event(DebugEvent{
+        .type = DebugEventType::ScriptError,
+        .location = location,
+        .script = location.file,
+        .message = std::wstring{message}
+    });
+}
+
+void DebugSiteBridge::process_stopped(std::wstring_view message) {
+    session_.debugger_event(DebugEvent{
+        .type = DebugEventType::ProcessStopped,
+        .message = std::wstring{message}
+    });
+}
+
+} // namespace wshdbg::windows
