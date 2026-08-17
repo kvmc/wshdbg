e35c383da5b1a6b636ccf9dbd0efe453f242dde2
kvmc <kvmc@users.noreply.github.com>
2026-08-17 16:23:04 -0400

    Add Active Script debug site scaffold

---
+++ src/windows/debug_site.cpp
@@ -0,0 +1,15 @@
+// Active Scripting debugger integration begins here.
+//
+// Responsibilities:
+// - IActiveScriptSiteDebug implementation
+// - Source document registration
+// - Breakpoint binding callbacks
+// - Debug event translation into wshdbg-core events
+
+#include "wshdbg/debugger.hpp"
+
+namespace wshdbg::windows {
+
+// Initial scaffold. COM debugger interfaces will be implemented incrementally.
+
+}
