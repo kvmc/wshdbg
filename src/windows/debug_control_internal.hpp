728bf6c6ee67c13dde18da3646e6233a28fa0490
kvmc <kvmc@users.noreply.github.com>
2026-08-18 16:13:00 -0400

    Store stack snapshots in debug control

--- src/windows/debug_control_internal.hpp
+++ src/windows/debug_control_internal.hpp
@@ -4,14 +4,17 @@
 #include <activdbg.h>
 #include <condition_variable>
 #include <mutex>
+#include <vector>
 
 namespace wshdbg::windows {
 
 struct DebugControl::Impl {
     bool capture(IRemoteDebugApplicationThread* thread, std::wstring& error) noexcept;
+    void set_stack(std::vector<StackFrameInfo> frames);
     void complete() noexcept;
     DebugWaitResult wait(std::chrono::milliseconds timeout);
     bool paused() const noexcept;
+    std::vector<StackFrameInfo> stack() const;
     bool resume(ResumeAction action, std::wstring& error) noexcept;
 
 private:
@@ -21,6 +24,7 @@ private:
     std::condition_variable cv_;
     DWORD application_cookie_{0};
     DWORD thread_cookie_{0};
+    std::vector<StackFrameInfo> stack_;
     bool paused_{false};
     bool completed_{false};
 };
